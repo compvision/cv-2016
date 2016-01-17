b93ff385c44defa658393db670d302a5c74e538a
Nick N <nick.thenebels@gmail.com>
2016-01-16 16:57:00 -0800

    added .gitignore

---
+++ build/src/CMakeFiles/catapult.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catapult.dir/main.cpp.o"
+  "CMakeFiles/catapult.dir/Target.cpp.o"
+  "CMakeFiles/catapult.dir/TargetDetector.cpp.o"
+  "CMakeFiles/catapult.dir/TargetProcessor.cpp.o"
+  "CMakeFiles/catapult.dir/NetworkController.cpp.o"
+  "CMakeFiles/catapult.dir/VideoDevice.cpp.o"
+  "CMakeFiles/catapult.dir/GUIManager.cpp.o"
+  "CMakeFiles/catapult.dir/CmdLineInterface.cpp.o"
+  "CMakeFiles/catapult.dir/AppConfig.cpp.o"
+  "catapult.pdb"
+  "catapult"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/catapult.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
